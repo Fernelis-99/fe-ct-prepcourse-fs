645ee2dac45702561f5a2ed63e04c0a93bc3c835
fernelisc14 <fernelisc99@gmail.com>
2023-05-31 21:55:57 -0500

    first commit

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -1,16 +1,36 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*3️⃣ EJERCICIO 03 3️⃣*/
 
-function sonIguales(x, y) {
+function sonIguales(x = 6, y = 9) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   if(x > y && x < y){
+      sonIguales = true;
+   }
+   else{
+      sonIguales = false;
+   }
+      return sonIguales;
+
 }
 
-function tienenMismaLongitud(str1, str2) {
+
+function tienenMismaLongitud(str1 = 'perfume', str2 = 'perfumes') {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   const longitud1 = str1.length;
+   const longitud2 = str2.length;
+
+   if(longitud1 === longitud2){
+      tienenMismaLongitud = true;
+   }
+   else{
+      tienenMismaLongitud = false;
+   }
+
+   return tienenMismaLongitud;
 }
 
 function menosQueNoventa(num) {
